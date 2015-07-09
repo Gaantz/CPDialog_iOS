e7b9acc44613aa63b40acc5e4085f46d36e11f8b
cristian <cp@mambo.pe>
2015-07-09 11:46:35 -0500

    Initial Commit

---
+++ SuraDialog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuraDialog
+//
+//  Created by Cristian on 9/07/15.
+//  Copyright (c) 2015 Cristian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
